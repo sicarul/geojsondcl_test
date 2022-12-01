6a7353eb0942ed97660d071d7576b857280dfcf3
Pablo Seibelt <pabloseibelt@sicarul.com>
2022-12-01 13:23:21 -0300

    Fix coordinats

--- generate_coordinates.py
+++ generate_coordinates.py
@@ -8,22 +8,32 @@ with open('all_tiles.json') as tiles_f:
 
     i=0
 
-    for quadrant_X in range(30):
-      x_start = (quadrant_X - 15) * 10
-      x_end = x_start + 9
-      if x_end == 149:
-        x_end = 150
+    GRID_SIZE = 5
+    GRID_LENGTH = int(300 / GRID_SIZE)
 
-      for quadrant_Y in range(30):
-        y_start = (quadrant_Y - 15) * 10
-        y_end = y_start + 9
+    for quadrant_X in range(GRID_LENGTH):
+      x_start = int((quadrant_X - (GRID_LENGTH / 2)) * GRID_SIZE)
+
+      for quadrant_Y in range(GRID_LENGTH):
+        y_start = int((quadrant_Y - (GRID_LENGTH / 2)) * 5)
+        x_end = int(x_start + GRID_SIZE - 1)
+        y_end = int(y_start + GRID_SIZE - 1)
+        if x_end == 149:
+          x_end = 150
         if y_end == 149:
           y_end = 150
+        
+        # Top aetherian border
+        if x_start >= 60 and y_end == 150:
+          y_end = 158
+        if y_start >= 60 and x_end == 150:
+          x_end = 163
 
         coord_x_start = float(round(Decimal(x_start)/1000,4))
         coord_y_start = float(round(Decimal(y_start)/1000,4))
         coord_x_end = float(round(Decimal(x_end)/1000,4))
         coord_y_end = float(round(Decimal(y_end)/1000,4))
+
 
         features.append(
           {
@@ -51,6 +61,7 @@ with open('all_tiles.json') as tiles_f:
               ]]
             },
             "properties": {
+              "grid_id": i,
               "coord_start": f"{x_start}|{y_start}",
               "coord_end": f"{x_end}|{y_end}"
             }
